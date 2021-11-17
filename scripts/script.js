4885287e2ecdd9e8a5d92fe5d56f3e4f7ad034f4
Evandro <evandrosilva558@hotmail.com>
2021-11-17 09:00:52 -0300

    adicionando os arquivos

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let a;
+
+function trocaCor() {
+    a.style.backgroudColor = "hsl: 0, 100%, 100%";
+    trocaCor.addEventListener
+}
+function trocaCor(){
+    var cor = document.getElementsByClassName("a").value;
+    document.getElementsByClassName("boxum").style.backgroundColor = cor;
+}
